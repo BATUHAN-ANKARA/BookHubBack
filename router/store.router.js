7a3d9774635ce841442ead1ef00a6e5ed4655553
BATUHAN ANKARA <56346846+BATUHAN-ANKARA@users.noreply.github.com>
2023-05-22 12:23:46 +0300

    cart routing

--- router/store.router.js
+++ router/store.router.js
@@ -4,6 +4,10 @@ const controller = require("../controllers/index");
 
 router.post("/likeBook", controller.likedController.likeBook);
 router.get("/getLikedBook/:id", controller.likedController.getLikedById);
+router.post("/addToCart", controller.cartController.addToCart);
+router.get("/getCart", controller.cartController.getCart);
+router.post("/removeFromCart", controller.cartController.removeFromCart);
+router.put("/updateCartItemQuantity", controller.cartController.updateCartItemQuantity);
 
 module.exports = {
   store: router
