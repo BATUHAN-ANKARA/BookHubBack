d2e96353a7501183967666dc03125dbce645dd85
BATUHAN ANKARA <56346846+BATUHAN-ANKARA@users.noreply.github.com>
2023-05-22 12:23:18 +0300

    cart services

---
+++ services/cart.service.js
@@ -0,0 +1,82 @@
+const Cart = require("../models/cart.model");
+const cartDal = require("../dal/index");
+const utils = require("../utils/index");
+
+exports.addToCart = async (req) => {
+  try {
+    const { userId, books } = req.body;
+    const cart = await cartDal.cart.findOne({ userId });
+    if (!cart) {
+      // Kullanıcının sepeti henüz oluşturulmamışsa, yeni bir sepet oluşturuyoruz
+      const newCart = new Cart({ userId, books });
+      await newCart.save();
+      return newCart;
+    } else {
+      cart.books.push(...books);
+      await cart.save();
+      return cart;
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.getCart = async (req) => {
+  const { userId } = req.query;
+  console.log(req.query);
+  try {
+    const cart = await Cart.findOne({
+      userId: userId
+    }).populate("books.bookId", "quantity price"); // Ürün bilgilerini getirmek için populate kullanıyoruz
+
+    if (!cart) {
+      throw new Error("Sepet bulunamadı");
+    }
+    return cart;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+exports.removeFromCart = async (req) => {
+  const { userId, bookId } = req.body;
+  try {
+    const cart = await cartDal.cart.findOne({ userId });
+    if (!cart) {
+      throw new Error("Sepet bulunamadı");
+    }
+    console.log("before=>", cart.books);
+    cart.books = cart.books.filter((book) => book.bookId.toString() !== bookId);
+    console.log("after=>", cart.books);
+    await cart.save();
+
+    return cart;
+  } catch (error) {
+    console.error("Sepetten çıkarma hatası:", error);
+    throw new Error("Sepetten çıkarma hatası:", error);
+  }
+};
+
+exports.updateCartItemQuantity = async (req) => {
+  const { userId, bookId, quantity } = req.body;
+  try {
+    const cart = await cartDal.cart.findOne({ userId });
+    if (!cart) {
+      throw new Error("Sepet bulunamadı");
+    }
+    const bookIndex = cart.books.findIndex(
+      (book) => book.bookId.toString() === bookId
+    );
+    if (bookIndex === -1) {
+      throw new Error("Ürün sepetinizde bulunamadı");
+    }
+    cart.books[bookIndex].quantity = quantity;
+    await cart.save();
+    return cart;
+  } catch (error) {
+    console.error("Ürün miktarını güncelleme hatası:", error);
+    throw new Error("Ürün miktarını güncelleme hatası:", error);
+  }
+};
+
+
--- services/index.js
+++ services/index.js
@@ -3,6 +3,7 @@ const ordersService = require("./orders.service");
 const fileService = require("./file.service");
 const likedService = require("./liked.service");
 const booksService = require("./books.service");
+const cartService = require("./cart.service");
 
 module.exports = {
   user: userService,
@@ -10,4 +11,5 @@ module.exports = {
   file: fileService,
   liked: likedService,
   books: booksService,
+  cart: cartService,
 };
