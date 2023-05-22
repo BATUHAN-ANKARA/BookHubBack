5f4d5132002ada87b0f83fea366a2422408084c7
BATUHAN ANKARA <56346846+BATUHAN-ANKARA@users.noreply.github.com>
2023-05-22 12:25:05 +0300

    cart controllers

---
+++ controllers/cart.controller.js
@@ -0,0 +1,124 @@
+const utils = require("../utils/index");
+const baseResponse = require("../dto/baseresponse.dto");
+const { StatusCodes } = require("http-status-codes");
+const cartService = require("../services/index");
+
+exports.addToCart = async (req, res) => {
+  try {
+    const isInvalid = utils.helpers.handleValidation(req);
+    if (isInvalid) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ...baseResponse, ...isInvalid });
+      return;
+    }
+    const json = await cartService.cart.addToCart(req);
+    res.status(StatusCodes.OK).json({
+      ...baseResponse,
+      data: json,
+      success: true,
+      timestamp: Date.now(),
+      code: StatusCodes.OK
+    });
+  } catch (error) {
+    utils.helpers.logToError(error, req);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      ...baseResponse,
+      success: false,
+      error: true,
+      timestamp: Date.now(),
+      message: error.message,
+      code: StatusCodes.INTERNAL_SERVER_ERROR
+    });
+  }
+};
+
+exports.getCart = async (req, res) => {
+  try {
+    const isInvalid = utils.helpers.handleValidation(req);
+    if (isInvalid) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ...baseResponse, ...isInvalid });
+      return;
+    }
+    const json = await cartService.cart.getCart(req);
+    res.status(StatusCodes.OK).json({
+      ...baseResponse,
+      data: json,
+      success: true,
+      timestamp: Date.now(),
+      code: StatusCodes.OK
+    });
+  } catch (error) {
+    utils.helpers.logToError(error, req);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      ...baseResponse,
+      success: false,
+      error: true,
+      timestamp: Date.now(),
+      message: error.message,
+      code: StatusCodes.INTERNAL_SERVER_ERROR
+    });
+  }
+};
+
+exports.removeFromCart = async (req, res) => {
+  try {
+    const isInvalid = utils.helpers.handleValidation(req);
+    if (isInvalid) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ...baseResponse, ...isInvalid });
+      return;
+    }
+    const json = await cartService.cart.removeFromCart(req);
+    res.status(StatusCodes.OK).json({
+      ...baseResponse,
+      data: json,
+      success: true,
+      timestamp: Date.now(),
+      code: StatusCodes.OK
+    });
+  } catch (error) {
+    utils.helpers.logToError(error, req);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      ...baseResponse,
+      success: false,
+      error: true,
+      timestamp: Date.now(),
+      message: error.message,
+      code: StatusCodes.INTERNAL_SERVER_ERROR
+    });
+  }
+};
+
+exports.updateCartItemQuantity = async (req, res) => {
+  try {
+    const isInvalid = utils.helpers.handleValidation(req);
+    if (isInvalid) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ ...baseResponse, ...isInvalid });
+      return;
+    }
+    const json = await cartService.cart.updateCartItemQuantity(req);
+    res.status(StatusCodes.OK).json({
+      ...baseResponse,
+      data: json,
+      success: true,
+      timestamp: Date.now(),
+      code: StatusCodes.OK
+    });
+  } catch (error) {
+    utils.helpers.logToError(error, req);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      ...baseResponse,
+      success: false,
+      error: true,
+      timestamp: Date.now(),
+      message: error.message,
+      code: StatusCodes.INTERNAL_SERVER_ERROR
+    });
+  }
+};
--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,13 @@ const userController = require("./user.controller");
 const likedController = require("./liked.controller");
 const ordersController = require("./orders.controller");
 const booksController = require("./books.controller");
+const cartController = require("./cart.controller");
 
 module.exports = {
   authController,
   userController,
   likedController,
   ordersController,
-  booksController
+  booksController,
+  cartController,
 };
