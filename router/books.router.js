b433284a2d1cab538aca5c88dba79892d23eed36
BATUHAN ANKARA <56346846+BATUHAN-ANKARA@users.noreply.github.com>
2023-05-23 11:39:56 +0300

    like and commerce steps routings

--- router/books.router.js
+++ router/books.router.js
@@ -6,6 +6,7 @@ const userValidator = require("../validations/index");
 router.post("/addBook", controller.booksController.addBook);
 router.put("/updateQuentity", controller.booksController.updateQuentity);
 router.get("/getBooksByCategory", controller.booksController.getBooksByCategory);
+router.get("/getBookById", controller.booksController.getBookById);
 
 module.exports = {
     books: router
--- router/store.router.js
+++ router/store.router.js
@@ -4,10 +4,12 @@ const controller = require("../controllers/index");
 
 router.post("/likeBook", controller.likedController.likeBook);
 router.get("/getLikedBook/:id", controller.likedController.getLikedById);
+router.get("/isLiked", controller.likedController.isLiked);
 router.post("/addToCart", controller.cartController.addToCart);
 router.get("/getCart", controller.cartController.getCart);
 router.post("/removeFromCart", controller.cartController.removeFromCart);
 router.put("/updateCartItemQuantity", controller.cartController.updateCartItemQuantity);
+router.post("/clearCart", controller.cartController.clearCart);
 
 module.exports = {
   store: router
